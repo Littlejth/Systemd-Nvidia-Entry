c856f6b3cdab9d38c8f5cca5b6e56f4cf22841b1
John Hertzog <littlejth@mykolab.com>
2018-06-12 15:16:52 -0500

    Remove bbswitch support as it's buggy on certain laptops (mine included)

--- config-optimus.sh
+++ config-optimus.sh
@@ -6,12 +6,10 @@ fi
 modprobe bbswitch ## starting bbswitch
 
 if ! [[ `lsmod | grep nvidia` == "" ]]; then ## Nvidia
-	tee /proc/acpi/bbswitch <<<ON
 	if ! [[ -e /etc/X11/xorg.conf.d/00-ldm.conf ]]; then
 		cp /opt/Systemd-Nvidia-Entry/00-ldm.conf /etc/X11/xorg.conf.d/00-ldm.conf -f
 	fi
 else ## Intel
-	tee /proc/acpi/bbswitch <<<OFF
 	if [[ -e /etc/X11/xorg.conf.d/00-ldm.conf ]]; then
 		mv /etc/X11/xorg.conf.d/00-ldm.conf /opt/Systemd-Nvidia-Entry/00-ldm.conf -f
 	fi
